a9b554ac87d848f96c72ba84a6bf42ebd4bc9f9e
JotaTR <JotaTR88@gmail.com>
2016-12-21 01:49:42 +0000

    [FIX] download and upload bakup OK

--- website/Admin/php/backups/.deleted/1482188066_backup_1482187661.sql
+++
@@ -1,5 +0,0 @@
-UPDATE requilib_website.destaque 
-				  SET titulo = 'Fisioterapeutas da R\'equilibrius participaram no Seminário Instabilidade Dinâmica no Membro Inferiordsadsa',
-					  resumo = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.',
-					  texto = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.'
-				WHERE id = '10';
--- website/Admin/php/backups/.deleted/1482192759_backup_1482192754_auto.sql
+++
@@ -1,9 +0,0 @@
-UPDATE requilib_website.destaque 
-				  SET titulo = 'Fisioterapeutas da R\'equilibrius participaram no Seminário Instabilidade Dinâmica no Membro Inferior',
-					  resumo = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.',
-					  texto = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.'
-				WHERE id = '2'UPDATE requilib_website.destaque 
-				  SET titulo = 'Fisioterapeutas da R\'equilibrius participaram no Seminário Instabilidade Dinâmica no Membro Inferioroiopioipoipo',
-					  resumo = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.',
-					  texto = 'No passado dia 12 de Novembro, os Fisioterapeutas João Rocha Vaz e Raúl Oliveira estiveram presentes no Seminário Instabilidade Dinâmica no Membro Inferior, organizado pelo Laboratório de Comportamento Motor da FMH.'
-				WHERE id = '2'
